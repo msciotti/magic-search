58c9f75d15be34ea479e5296808d5f7fff3b8584
Mason Sciotti <mason.sciotti@gmail.com>
2024-12-02 16:06:27 -0500

    We can't all be geniuses

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,6 @@ export default {
 
 async function handleRequest(request: Request, env: any): Promise<Response> {
   const url = new URL(request.url);
-  
-  if (url.pathname === '/token') {
-    return new Response(JSON.stringify({token: env.DISCORD_BOT_TOKEN}), {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  }
 
   if (url.pathname === '/search') {
     return await getMagicCard(request, env);
